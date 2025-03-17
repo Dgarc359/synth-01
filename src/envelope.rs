c97d87a4883fd2159737363ce26b411de85bf691
David Garcia <87497958+Dgarc359@users.noreply.github.com>
2025-03-16 20:22:30 -0400

    adjust calculation for current value of envelope

--- src/envelope.rs
+++ src/envelope.rs
@@ -7,37 +7,17 @@ pub enum AdsrEnvelopeStates {
 }
 
 #[derive(Clone, Copy)]
-pub struct EnvelopeSingleStateConfig { 
+pub struct EnvelopeSingleStateConfig {
     pub state_type: AdsrEnvelopeStates,
-    pub duration: u16, 
-    pub target: f32, 
-    pub start_value: f32,
+    pub duration: u16,
+    pub target: f32,
     pub next_state: Option<AdsrEnvelopeStates>,
 }
 
-pub struct MinMax {
-    min: f32,
-    max: f32,
-}
-
 impl EnvelopeSingleStateConfig {
-    pub fn new(state_type: AdsrEnvelopeStates, start_value: f32, target: f32, duration: u16, next_state: Option<AdsrEnvelopeStates>, ) -> Self {
+    pub fn new(state_type: AdsrEnvelopeStates, target: f32, duration: u16, next_state: Option<AdsrEnvelopeStates>, ) -> Self {
         Self {
-            state_type, duration, target, next_state, start_value
-        }
-    }
-
-    /**
-     * Get the minimum and maximum between the envelopes start value and the target value
-     * the goal of this is to always have the min / max and not have to worry the user of the envelope
-     * with calculating min / max for normalization
-     * 
-     * User should basically not worry about what the math behind normalization IS
-     */
-    pub fn get_min_max(&self) -> MinMax {
-        MinMax {
-            min: self.start_value.min(self.target),
-            max: self.start_value.max(self.target),
+            state_type, duration, target, next_state
         }
     }
 }
@@ -53,7 +33,6 @@ pub struct AdsrEnvelopeConfig {
 
 #[derive(Clone, Copy)]
 pub struct AdsrEnvelope {
-    start_val: f32,
     current_val: f32,
     current_duration: u16,
     current_state: Option<AdsrEnvelopeStates>,
@@ -79,51 +58,44 @@ impl AdsrEnvelope {
         self.current_state
     }
 
-    fn next(&self, current_config: EnvelopeSingleStateConfig, val: f32) -> f32 {
+    fn next(&self, current_config: EnvelopeSingleStateConfig, val: u16) -> u16 {
         match current_config.state_type {
-            AdsrEnvelopeStates::Attack => {
-                val.wrapping_add(1).min(current_config.target)
-            }
-            AdsrEnvelopeStates::Delay => { val.wrapping_sub(1).max(current_config.target)}
+            AdsrEnvelopeStates::Attack => { val.wrapping_add(1).min(current_config.duration) }
+            AdsrEnvelopeStates::Delay => { val.wrapping_sub(1).max(current_config.duration) }
             AdsrEnvelopeStates::Sustain => { val }
-            AdsrEnvelopeStates::Release => { val.wrapping_sub(1).max(current_config.target) }
+            AdsrEnvelopeStates::Release => { val.wrapping_sub(1).max(current_config.duration) }
         }
     }
 
 
-    //  each envelope state needs to eventually implement its own strategy for modifying the counter to hit its target 
-    pub fn generate_next_value(&mut self) { 
+    //  each envelope state needs to eventually implement its own strategy for modifying the counter to hit its target
+    pub fn generate_next_value(&mut self) {
         let current_config = self.get_current_config_from_state();
 
         if let Some(current_config) = current_config {
             match current_config.state_type {
                 AdsrEnvelopeStates::Sustain {..} => {  },
-                _ => { 
+                _ => {
                     if self.current_val == current_config.target {
                         // we need to transition to the next state
                         let next_config = current_config.next_state.clone();
-                        self.current_val = self.next(current_config, self.current_val);
+                        self.current_duration = self.next(current_config, self.current_duration);
+                        self.current_val = crate::util::normalize(self.current_duration, current_config.duration, 0);
 
                         self.set_current_config_state(next_config);
                     } else {
                         // if we havent hit the target yet, then do the value next!
-                        self.current_val = self.next(current_config, self.current_val);
+                        self.current_duration = self.next(current_config, self.current_duration);
 
                     }
                 }
             }
-        } 
+        }
      }
 
-
-    pub fn denormalize_current_value(&self) -> u16 {
-
-    }
-
     pub fn get_normalized_current_value(&self) -> f32 {
-        if let Some(current_config) = self.get_current_config_from_state() {
-            let min_max = current_config.get_min_max();
-            crate::util::normalize_f32(self.current_val, min_max.max, min_max.min)
+        if self.get_current_config_from_state().is_some() {
+            self.current_val
         } else {
             1.
         }
@@ -131,7 +103,6 @@ impl AdsrEnvelope {
 
     pub fn new(current_duration: u16, start_val: f32, current_state: Option<AdsrEnvelopeStates>, state_config: AdsrEnvelopeConfig) -> Self {
         Self {
-            start_val,
             state_config,
             current_state,
             current_val: start_val,
