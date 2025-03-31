109820105e213142eee668bb70042906afafd19e
dgarc359 <87497958+Dgarc359@users.noreply.github.com>
2025-03-31 00:05:17 -0400

    init output frequency in rs code

--- esp32-c3/src/main.rs
+++ esp32-c3/src/main.rs
@@ -1,6 +1,9 @@
 use embedded_midi::MidiIn;
 use esp_idf_svc::hal;
+use esp_idf_svc::hal::delay::Ets;
 use esp_idf_svc::hal::prelude::*;
+
+use esp_idf_hal::delay::Ets;
 
 fn main() {
     // It is necessary to call this function once. Otherwise some patches to the runtime
@@ -39,6 +42,20 @@ fn main() {
         &uart_config
     ).expect("Unable to start uart driver!!!!!!!!");
 
+    // read_midi(uart_driver);
+
+    // we're outputting on the (physical) other side of the esp32-c3 board
+    // just to make the wiring less cluttered
+    let output_pin = hal::gpio::PinDriver::output(peripherals.pins.gpio2).expect("Failed to create output pin driver!!!");
+
+    let middle_c_frequency = 261;
+    let note_duration = 1000;
+
+    output_frequency(middle_c_frequency, note_duration, output_pin);
+}
+
+// this will block the caller indefinitely.
+fn read_midi(uart_driver: hal::uart::UartRxDriver<'_>) {
     let mut midi_in = MidiIn::new(uart_driver);
 
     loop {
@@ -46,4 +63,22 @@ fn main() {
             println!("event: {:#?}", event)
         }
     }
+}
+
+fn output_frequency(frequency: u32, note_duration: u32, mut outpin: hal::gpio::PinDriver<'_, impl hal::gpio::OutputPin, hal::gpio::Output>) {
+    // from arduino example, we subtract by 7 microseconds to make up
+    // for digital write overhead. This might not be necessary
+    // with esp
+    let half_period = (500_000 / frequency) - 7;
+    loop {
+        outpin.set_high().expect("Couldn't set pin high");
+        delay_microseconds(half_period);
+        outpin.set_low();
+        delay_microseconds(half_period);
+    }
+}
+
+
+fn delay_microseconds(microseconds: u32) {
+    Ets::delay_us(microseconds);
 }
